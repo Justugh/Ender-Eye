aefd7c4290c88a623ae160ec489e456958103349
Justin <justughv@gmail.com>
2019-04-06 04:36:23 -0400

    Implement Locale System

--- src/main/java/net/justugh/endereye/EnderEye.java
+++ src/main/java/net/justugh/endereye/EnderEye.java
@@ -1,20 +1,31 @@
 package net.justugh.endereye;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import lombok.Getter;
+import net.justugh.endereye.locale.Locale;
 import net.justugh.endereye.managers.xray.XRayManager;
 import net.justugh.endereye.managers.xray.commands.XRayStatsCommand;
+import org.apache.commons.io.FileUtils;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 @Getter
 public class EnderEye extends JavaPlugin {
 
     private static EnderEye instance;
+    private Locale locale;
 
     private XRayManager xRayManager;
 
     @Override
     public void onEnable() {
         instance = this;
+        loadLocale();
         saveDefaultConfig();
 
         xRayManager = new XRayManager();
@@ -29,6 +40,35 @@ public class EnderEye extends JavaPlugin {
 
     public static EnderEye getInstance() {
         return instance;
+    }
+
+    /**
+     * Load the Locale file to memory.
+     */
+    private void loadLocale() {
+        File localeFile = new File(getDataFolder() + File.separator + "locale.json");
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        if(localeFile.exists()) {
+            try {
+                Locale locale = gson.fromJson(new FileReader(localeFile), Locale.class);
+
+                FileUtils.writeStringToFile(localeFile, gson.toJson(locale)); // Make sure the Locale has all the latest variables.
+
+                this.locale = gson.fromJson(new FileReader(localeFile), Locale.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                FileUtils.writeStringToFile(localeFile, gson.toJson(new Locale()));
+
+                this.locale = gson.fromJson(new FileReader(localeFile), Locale.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
---
+++ src/main/java/net/justugh/endereye/locale/Locale.java
@@ -0,0 +1,17 @@
+package net.justugh.endereye.locale;
+
+import lombok.Getter;
+
+@Getter
+public class Locale {
+
+    // General
+    private String prefix = "&9&lEnder Eye &8»&9";
+    private String senderMustBePlayer = "%prefix% Only players can use this command!";
+
+    // X-Ray
+    private String xrayNoWorldData = "%prefix% There is no data for %player% in the world %world%.";
+    private String xrayDataTitle = "&9X-Ray Data for &b%player%&8:";
+    private String xrayDataItem = "&b%ore_type% &8(&9Amount Mined&8: &b%amount_mined%&8, &9Veins Mined&8: &b%veins_mined%&8) (&b%percentage%%&8)";
+
+}
