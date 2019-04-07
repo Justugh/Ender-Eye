d1e8a29f4c3d5e0d88ba1edc060a2d35d3c05406
Justin <justughv@gmail.com>
2019-04-07 00:16:27 -0400

    Use JSON based Config system

--- src/main/java/net/justugh/endereye/EnderEye.java
+++ src/main/java/net/justugh/endereye/EnderEye.java
@@ -3,6 +3,7 @@ package net.justugh.endereye;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import lombok.Getter;
+import net.justugh.endereye.config.Config;
 import net.justugh.endereye.locale.Locale;
 import net.justugh.endereye.managers.xray.XRayManager;
 import net.justugh.endereye.managers.xray.commands.XRayStatsCommand;
@@ -10,7 +11,6 @@ import org.apache.commons.io.FileUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -18,6 +18,7 @@ import java.io.IOException;
 public class EnderEye extends JavaPlugin {
 
     private static EnderEye instance;
+    private Config configuration;
     private Locale locale;
 
     private XRayManager xRayManager;
@@ -25,8 +26,8 @@ public class EnderEye extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+        loadConfig();
         loadLocale();
-        saveDefaultConfig();
 
         xRayManager = new XRayManager();
         getCommand("xraycheck").setExecutor(new XRayStatsCommand());
@@ -40,6 +41,35 @@ public class EnderEye extends JavaPlugin {
 
     public static EnderEye getInstance() {
         return instance;
+    }
+
+    /**
+     * Load the Locale file to memory.
+     */
+    private void loadConfig() {
+        File configFile = new File(getDataFolder() + File.separator + "config.json");
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        if(configFile.exists()) {
+            try {
+                Config config = gson.fromJson(new FileReader(configFile), Config.class);
+
+                FileUtils.writeStringToFile(configFile, gson.toJson(config)); // Make sure the Config has all the latest variables.
+
+                this.configuration = gson.fromJson(new FileReader(configFile), Config.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                FileUtils.writeStringToFile(configFile, gson.toJson(new Config()));
+
+                this.configuration = gson.fromJson(new FileReader(configFile), Config.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
---
+++ src/main/java/net/justugh/endereye/config/Config.java
@@ -0,0 +1,10 @@
+package net.justugh.endereye.config;
+
+import lombok.Getter;
+
+@Getter
+public class Config {
+
+    private long xraySaveTime = 30;
+
+}
