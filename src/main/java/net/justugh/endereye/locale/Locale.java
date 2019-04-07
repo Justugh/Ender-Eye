2780fb149d7d8061d841188df6ab257366ec786e
Justin <justughv@gmail.com>
2019-04-07 00:16:05 -0400

    Add command usage variable

--- src/main/java/net/justugh/endereye/locale/Locale.java
+++ src/main/java/net/justugh/endereye/locale/Locale.java
@@ -10,6 +10,7 @@ public class Locale {
     private String senderMustBePlayer = "%prefix% Only players can use this command!";
 
     // X-Ray
+    private String xrayCommandUsage = "%prefix% Usage: /xraycheck <player> (world)";
     private String xrayNoWorldData = "%prefix% There is no data for %player% in the world %world%.";
     private String xrayDataTitle = "&9X-Ray Data for &b%player%&8:";
     private String xrayDataItem = "&b%ore_type% &8(&9Amount Mined&8: &b%amount_mined%&8, &9Veins Mined&8: &b%veins_mined%&8) (&b%percentage%%&8)";
